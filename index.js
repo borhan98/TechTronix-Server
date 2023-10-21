f28df28360e6f9eefc09ea59ffe583600acf918c
borhan98 <borhanuddindns420@gmail.com>
2023-10-21 20:11:18 +0600

    Loading Problem Solved

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // MiddleWare
@@ -23,12 +23,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
 
     const productCollection = client.db("productDB").collection("products");
     const cartCollection = client.db("productDB").collection("cart");
 
-    
     /*--------------- Products related program ----------------*/
     // GET products from products
     app.get("/products", async (req, res) => {
@@ -101,7 +99,6 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
